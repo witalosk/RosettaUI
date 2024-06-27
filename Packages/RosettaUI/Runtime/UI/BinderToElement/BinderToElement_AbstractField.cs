29471fd3a5687cf22b442696f4c1cd6a2f07afea
witalosk <shun_september@live.jp>
2024-06-27 23:34:19 +0900

    feat: support to display list of abstract class

---
+++ Packages/RosettaUI/Runtime/UI/BinderToElement/BinderToElement_AbstractField.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace RosettaUI
+{
+    public static partial class BinderToElement
+    {
+        public static Element CreateAbstractFieldElement(LabelElement label, IBinder binder, bool typeChangeable = false, FieldOption? option = null)
+        {
+            var valueType = binder.ValueType;
+
+            if (BinderHistory.IsCircularReference(binder))
+            {
+                return CreateCircularReferenceElement(label, valueType);
+            }
+            if (ListBinder.IsListBinder(binder) && ListUtility.GetItemType(valueType).IsAbstract)
+            {
+                return CreateAbstractListView(label, binder, typeChangeable);
+            }
+            if (!valueType.IsAbstract)
+            {
+                return CreateFieldElement(label, binder, option ?? FieldOption.Default);
+            }
+
+            var candidateTypes = TypeUtility.GetConcreteTypesFromAbstractType(valueType).ToList();
+            if (candidateTypes.Count < 1)
+            {
+                return new CompositeFieldElement(label, new[] {
+                    new HelpBoxElement($"[{valueType}] No concrete type found.", HelpBoxType.Error)
+                }).SetInteractable(false);
+            }
+            object obj = binder.GetObject();
+            int selectedIdx = obj == null ? 0 : candidateTypes.IndexOf(obj.GetType()) + 1;
+            typeChangeable = typeChangeable && obj is not MonoBehaviour;
+            
+            return UI.Fold(
+                label, 
+                UI.Dropdown("Type", () => selectedIdx, typeChangeable ? idx => 
+                    {
+                        obj = idx == 0 ? null : Activator.CreateInstance(candidateTypes[idx - 1]);
+                        binder.SetObject(obj);
+                        selectedIdx = idx;
+                    } : null,
+                    new []{ "<null>" }.Concat(candidateTypes.Select(t => t.Name))
+                ),
+                UI.DynamicElementOnStatusChanged(
+                    () => selectedIdx, 
+                    idx => idx == 0 ? null : UI.Field(null, Binder.Create(obj, obj?.GetType()), option ?? FieldOption.Default)
+                )
+            );
+        }
+        
+        private static Element CreateAbstractListView(LabelElement label, IBinder binder, bool typeChangeable = false)
+        {
+            var option = (binder is IPropertyOrFieldBinder pfBinder)
+                ? new ListViewOption(
+                    reorderable: TypeUtility.IsReorderable(pfBinder.ParentBinder.ValueType, pfBinder.PropertyOrFieldName)
+                )
+                : ListViewOption.Default;
+
+            return UI.List(label, binder, (b, i)=> UI.AbstractField($"Item {i}", b, typeChangeable), option);
+        }
+    }
+}
---
+++ Packages/RosettaUI/Runtime/UI/UI_AbsreactField.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RosettaUI
+{
+    public static partial class UI
+    {
+        public static Element AbstractField<T>(Expression<Func<T>> targetExpression, in bool typeChangeable = false, FieldOption? option = null)
+        {
+            return AbstractField(ExpressionUtility.CreateLabelString(targetExpression), targetExpression, typeChangeable, option);
+        }
+
+        public static Element AbstractField<T>(LabelElement label, Expression<Func<T>> targetExpression, bool typeChangeable = false, in FieldOption? option = null)
+        {
+            var binder = UIInternalUtility.CreateBinder(targetExpression);
+            return AbstractField(label, binder, typeChangeable, option);
+        }
+
+        public static Element AbstractField<T>(Expression<Func<T>> targetExpression, Action<T> writeValue, bool typeChangeable = false, in FieldOption? option = null)
+            => AbstractField(ExpressionUtility.CreateLabelString(targetExpression), targetExpression.Compile(), writeValue, typeChangeable, option);
+        
+        public static Element AbstractField<T>(LabelElement label, Func<T> readValue, Action<T> writeValue, bool typeChangeable = false, in FieldOption? option = null)
+            => AbstractField(label, Binder.Create(readValue, writeValue), typeChangeable, option);
+
+        public static Element AbstractField(LabelElement label, IBinder binder, bool typeChangeable = false, in FieldOption? option = null)
+        {
+            var element = BinderToElement.CreateAbstractFieldElement(label, binder, typeChangeable, option ?? FieldOption.Default);
+            if (element != null) UIInternalUtility.SetInteractableWithBinder(element, binder);
+
+            return element;
+        }
+
+        public static Element AbstractFieldReadOnly<T>(Expression<Func<T>> targetExpression)
+            => AbstractFieldReadOnly(
+                ExpressionUtility.CreateLabelString(targetExpression),
+                ExpressionUtility.CreateReadFunc(targetExpression
+                ));
+
+        public static Element AbstractFieldReadOnly<T>(LabelElement label, Func<T> readValue)
+            => AbstractField(label, readValue, null);
+    }
+}
